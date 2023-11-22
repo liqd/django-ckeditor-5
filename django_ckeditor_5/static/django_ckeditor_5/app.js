df60371c63d5d5daec99a983188413f8c95402bf
Julian Dehm <j.dehm@liqd.net>
2023-11-22 12:49:45 +0100

    overwrite mediaEmbed.providers with a custom youtube version which adds rel=0 and limit providers to youtube and vimeo

--- django_ckeditor_5/static/django_ckeditor_5/app.js
+++ django_ckeditor_5/static/django_ckeditor_5/app.js
@@ -88,6 +88,59 @@ function createEditors(element = document.body) {
             'fileTypes': upload_file_types
         };
 
+        // overwrite embed providers to add rel=0 to youtube videos
+        // this limits videos to youtube and vimeo
+        if(config.mediaEmbed) {
+            config.mediaEmbed.providers = [
+                {
+                    name: 'youtube',
+                    url: [
+                            /^(?:m\.)?youtube\.com\/watch\?v=([\w-]+)(?:&t=(\d+))?/,
+                            /^(?:m\.)?youtube\.com\/v\/([\w-]+)(?:\?t=(\d+))?/,
+                            /^youtube\.com\/embed\/([\w-]+)(?:\?start=(\d+))?/,
+                            /^youtu\.be\/([\w-]+)(?:\?t=(\d+))?/
+                    ],
+                    html: match => {
+                        const id = match[ 1 ];
+                        const time = match[ 2 ];
+
+                        return (
+                            '<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
+                                    `<iframe src="https://www.youtube.com/embed/${ id }?rel=0${ time ? `&start=${ time }` : '' }" ` +
+                                            'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+                                            'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
+                                    '</iframe>' +
+                            '</div>'
+                        );
+                    }
+                },
+                {
+                    name: 'vimeo',
+                    url: [
+                            /^vimeo\.com\/(\d+)/,
+                            /^vimeo\.com\/[^/]+\/[^/]+\/video\/(\d+)/,
+                            /^vimeo\.com\/album\/[^/]+\/video\/(\d+)/,
+                            /^vimeo\.com\/channels\/[^/]+\/(\d+)/,
+                            /^vimeo\.com\/groups\/[^/]+\/videos\/(\d+)/,
+                            /^vimeo\.com\/ondemand\/[^/]+\/(\d+)/,
+                            /^player\.vimeo\.com\/video\/(\d+)/
+                    ],
+                    html: match => {
+                            const id = match[ 1 ];
+
+                            return (
+                                    '<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
+                                            `<iframe src="https://player.vimeo.com/video/${ id }" ` +
+                                                    'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
+                                                    'frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>' +
+                                            '</iframe>' +
+                                    '</div>'
+                            );
+                    }
+                },
+            ];
+        }
+
         ClassicEditor.create(
             editorEl,
             config
