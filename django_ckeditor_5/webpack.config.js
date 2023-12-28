8ef39a58e6668ebf8f21fdc49adfbaeaec2a329d
Julian Dehm <j.dehm@liqd.net>
2023-12-28 11:02:28 +0100

    webpack: add copy plugin and config to copy map files from vanilla-colorful
    This is required to make whitenoise work with django >= 4.2 as
    collectstatic will error.

--- django_ckeditor_5/webpack.config.js
+++ django_ckeditor_5/webpack.config.js
@@ -3,6 +3,7 @@
 const path = require( 'path' );
 const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+const CopyWebpackPlugin = require( 'copy-webpack-plugin' )
 const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
 
 module.exports = {
@@ -26,7 +27,61 @@ module.exports = {
         } ),
         new MiniCssExtractPlugin( {
             filename: 'styles.css'
-        } )
+        } ),
+        // required for django's ManifestStaticFilesStorage / whitenoise to work as it
+        // otherwise will complain about these files missing
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: './node_modules/vanilla-colorful/hex-input.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/components/color-picker.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/components/hue.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/components/hue.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/components/saturation.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/components/slider.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/entrypoints/hex.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/utils/compare.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/utils/convert.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/utils/dom.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/utils/math.js.map',
+                    to: '[name][ext]'
+                },
+                {
+                    from: './node_modules/vanilla-colorful/lib/utils/math.js.map',
+                    to: '[name][ext]'
+                },
+            ]
+        })
     ],
 
     module: {
